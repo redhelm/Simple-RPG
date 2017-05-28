631b3c1b89c8a8d0d71bb876764628d77d533749
Shay Buttaccio <ShayButtaccio@gmail.com>
2017-05-28 13:03:33 -0700

    UI to display stats

--- Assets/Scripts/StatDisplay.cs
+++ Assets/Scripts/StatDisplay.cs
@@ -1,8 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+public class StatDisplay : MonoBehaviour {
+
+    public enum StatName{
+        Vitality, Strength, Intelligence, Range, Block, Dodge, AvailablePoints
+    }
+
+    public StatName statName;
+    private string statNameString;
+    private Text myText;
+    private StatsManager statsManager;
+
+	// Use this for initialization
+	void Start () {
+        statNameString = statName.ToString();
+        statsManager = FindObjectOfType<StatsManager>().GetComponent<StatsManager>();
+        myText = GetComponent<Text>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		myText.text = statsManager.getStat(statNameString).ToString();
+    }
 }
