74b18a58b837e9beb16748bb0943cfad7282df55
Shay Buttaccio <ShayButtaccio@gmail.com>
2017-05-29 13:15:28 -0700

    Introducing Projectiles
    Added projectiles on the Block Training, but still does not go in the
    direction of the player, unfortunately. Need to research some more.

---
+++ Assets/Scripts/EmitterBehavior.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmitterBehavior : MonoBehaviour {
+
+    public GameObject projectile;
+    public GameObject playerObj;
+    public float projectileSpeed = 10f;
+    public float shotsPerSecond = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float prob = shotsPerSecond * Time.deltaTime;
+        if (Random.value < prob)
+        {
+            Fire();
+        }
+    }
+
+    void Fire() {
+        //GameObject obj = Instantiate(projectile, transform.position, Quaternion.identity) as GameObject;
+        GameObject obj = Instantiate(projectile, transform.position, Quaternion.LookRotation(Vector3.forward, playerObj.transform.position - transform.position)) as GameObject;
+        obj.GetComponent<Rigidbody2D>().velocity = new Vector2(1, projectileSpeed);
+    }
+
+}
