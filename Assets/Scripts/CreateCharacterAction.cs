63a16997bc39e9095536059594c1b2e8dcb108c1
Shay Buttaccio <ShayButtaccio@gmail.com>
2017-05-29 11:26:18 -0700

    Character Creation and Block Training Controls

---
+++ Assets/Scripts/CreateCharacterAction.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreateCharacterAction : MonoBehaviour {
+
+    private string textInput;
+    private PlayerInfo playerInfo;
+
+    void Awake() {
+        playerInfo = FindObjectOfType<PlayerInfo>().GetComponent<PlayerInfo>();
+    }
+
+    public void CreateCharacter() {
+        textInput = GetComponent<Text>().text;
+        playerInfo.SetPlayerName(textInput);
+    }
+}
---
+++ Assets/Scripts/PlayerBlockTraining.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBlockTraining : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.rotation = Quaternion.LookRotation(Vector3.forward, mousePos - transform.position);
+	}
+}
