5d423d1a2633bfb6a882b01b9a8b9259af38aeac
Shay Buttaccio <ShayButtaccio@gmail.com>
2017-09-02 16:57:50 -0700

    Added EmitterController for more control over fire rates, etc

---
+++ Assets/Scripts/Emitter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Emitter : MonoBehaviour {
+    
+    public float overrideProjectileSpeed = 0.0f;
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    }
+
+    public void Fire(GameObject projectile, float projectileSpeed) {
+
+        if (overrideProjectileSpeed > 0.0f)
+        {
+            projectileSpeed = overrideProjectileSpeed;
+        }
+
+        GameObject obj = Instantiate(projectile, transform.position, Quaternion.identity) as GameObject;
+        obj.gameObject.GetComponent<ProjectileBehavior>().projectileSpeed = projectileSpeed;
+    }
+
+}
---
+++ Assets/Scripts/EmitterController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmitterController : MonoBehaviour {
+
+    private Emitter[] emitters;
+    private float nextFire = 0.0f;
+
+    public GameObject projectileDamage;
+    public GameObject projectileBonus;
+    [Tooltip("Higher values result in more bonus projectiles.")]
+    public float bonusChance = 0.2f;
+    public float projectileSpeed = 10f;
+    public float avgFireRate = 2.0f; //shots per second
+    public float minFireRate = 0.5f;
+    
+
+	// Use this for initialization
+	void Start () {
+
+        emitters = FindObjectsOfType(typeof(Emitter)) as Emitter[];
+        Debug.Log("Found " + emitters.Length + " instances with this script attached");
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        float prob = avgFireRate * Time.deltaTime;
+        if (Random.value < prob)
+        {
+            if (Time.time > nextFire)
+            {
+                nextFire = Time.time + minFireRate;
+                Fire();
+            }
+            
+        }
+    }
+
+    void Fire()
+    {
+        GameObject projectile = projectileDamage;
+
+        if (Random.value <= bonusChance)
+        {
+            projectile = projectileBonus;
+        }
+
+        Emitter emitter = emitters[Random.Range(0, emitters.Length)];
+        
+        emitter.Fire(projectile, projectileSpeed);
+    }
+}
