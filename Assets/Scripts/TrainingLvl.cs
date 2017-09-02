36bd9f384168798578f972eccdb7689559dd4a0c
Shay Buttaccio <ShayButtaccio@gmail.com>
2017-09-02 15:37:59 -0700

    Added Progress Bar

--- Assets/Scripts/TrainingLvl.cs
+++ Assets/Scripts/TrainingLvl.cs
@@ -5,6 +5,10 @@ using UnityEngine.UI;
 
 public class TrainingLvl : MonoBehaviour {
 
+    public Texture2D barImg;
+    Rect bgBarRect;
+    Rect progressBarRect;
+
     public Text scoreText;
     public Text skillIncreaseText;
 
@@ -17,7 +21,15 @@ public class TrainingLvl : MonoBehaviour {
     private int statsScoreIncrement;
     private int totalStatsScore;
 
-    private int levelDifficultry;
+    private int difficultyLvl;
+
+    void Awake()
+    {
+        float barWidth = Screen.width * 0.5f; //made float so it's easy to change size if needed, instead of simply dividing by 2.
+        float marginX = (Screen.width - barWidth) / 2;
+        bgBarRect = new Rect(marginX, 12, barWidth, 24);
+        progressBarRect = new Rect(2 + marginX, 14, 0, 20);
+    }
 
     // Use this for initialization
     void Start () {
@@ -27,6 +39,14 @@ public class TrainingLvl : MonoBehaviour {
     public void Update () {
         scoreText.GetComponent<Text>().text = score.ToString();
         skillIncreaseText.GetComponent<Text>().text = totalStatsScore.ToString();
+
+        if (score >= 0)
+            progressBarRect.width = score * bgBarRect.width / max;
+    }
+
+    void OnGUI()
+    {
+        GUI.Box(bgBarRect, GUIContent.none);
     }
 
     public void resetScore(int rollover)
@@ -54,7 +74,7 @@ public class TrainingLvl : MonoBehaviour {
     public void increaseStatsScore()
     {
         totalStatsScore += statsScoreIncrement;
-        levelDifficultry++;
+        difficultyLvl++;
     }
 
     public void setStatsScoreIncrement(int statsScoreIncrement)
