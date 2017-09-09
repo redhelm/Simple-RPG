036fa68711497ec8cf33fe2579ba57d5dfe07a49
Shay Buttaccio <ShayButtaccio@gmail.com>
2017-09-09 10:12:07 -0700

    Algorithm for displaying total score gained transition
    This will display the total score, counting up from 0 within a specified
    fixed time frame.

--- Assets/Scripts/TrainingRoomStats.cs
+++ Assets/Scripts/TrainingRoomStats.cs
@@ -8,14 +8,50 @@ public class TrainingRoomStats : MonoBehaviour {
     public Text blockHighestCombo;
     private int i_blockHighestCombo;
 
+    public Text gainedScore;
+
+    int totalScore;
+    int currentScore;
+    float transTime;
+    int remainingScore;
+
 	// Use this for initialization
 	void Start () {
         i_blockHighestCombo = PlayerPrefs.GetInt("Block Highest Combo");
         blockHighestCombo.text = "x" + i_blockHighestCombo.ToString();
+
+        totalScore = 500;
+        currentScore = 0;
+        transTime = 1f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+        if (currentScore < totalScore)
+        {
+            
+            remainingScore = totalScore - currentScore;
+
+            transTime = transTime - Time.deltaTime;
+
+            if (transTime < 0)
+            {
+                transTime = 0;
+            }
+
+            float increment = remainingScore / (transTime / Time.deltaTime);
+
+            currentScore = (int)increment + currentScore;
+
+            if (currentScore > totalScore)
+            {
+                currentScore = totalScore;
+            }
+
+            gainedScore.text = currentScore.ToString();
+        }
+        
 		
 	}
 }
